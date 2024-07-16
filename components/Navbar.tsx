7861f779d42471dbdc74a26b1470bf1612845d1e
abdul8840 <abdul14941naseer@gmail.com>
2024-07-16 19:30:25 +0530

    create add to cart functionality and display product to cart page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,22 @@
-"use client"
+"use client";
 
-import { UserButton, useUser } from '@clerk/nextjs';
-import { CircleUserRound, Menu, ShoppingCart } from 'lucide-react';
-import Image from 'next/image';
-import Link from 'next/link';
-import { usePathname, useRouter } from 'next/navigation';
-import { useState } from 'react';
+import useCart from "@/lib/hooks/useCart";
+
+import { UserButton, useUser } from "@clerk/nextjs";
+import { CircleUserRound, Menu, Search, ShoppingCart } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import { useState } from "react";
 
 const Navbar = () => {
   const pathname = usePathname();
   const router = useRouter();
   const { user } = useUser();
+  const cart = useCart();
 
   const [dropdownMenu, setDropdownMenu] = useState(false);
+  const [query, setQuery] = useState("");
 
   return (
     <div className="sticky top-0 z-10 py-2 px-10 flex gap-2 justify-between items-center bg-white max-sm:px-2">
@@ -23,10 +27,43 @@ const Navbar = () => {
       <div className="flex gap-4 text-base-bold max-lg:hidden">
         <Link
           href="/"
-          className={`hover:text-red-1 ${pathname === '/' && 'text-red-1'}`}
+          className={`hover:text-red-1 ${
+            pathname === "/" && "text-red-1"
+          }`}
         >
           Home
         </Link>
+        <Link
+          href={user ? "/wishlist" : "/sign-in"}
+          className={`hover:text-red-1 ${
+            pathname === "/wishlist" && "text-red-1"
+          }`}
+        >
+          Wishlist
+        </Link>
+        <Link
+          href={user ? "/orders" : "/sign-in"}
+          className={`hover:text-red-1 ${
+            pathname === "/orders" && "text-red-1"
+          }`}
+        >
+          Orders
+        </Link>
+      </div>
+
+      <div className="flex gap-3 border border-grey-2 px-3 py-1 items-center rounded-lg">
+        <input
+          className="outline-none max-sm:max-w-[120px]"
+          placeholder="Search..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button
+          disabled={query === ""}
+          onClick={() => router.push(`/search/${query}`)}
+        >
+          <Search className="cursor-pointer h-4 w-4 hover:text-red-1" />
+        </button>
       </div>
 
       <div className="relative flex gap-3 items-center">
@@ -35,7 +72,7 @@ const Navbar = () => {
           className="flex items-center gap-3 border rounded-lg px-2 py-1 hover:bg-black hover:text-white max-md:hidden"
         >
           <ShoppingCart />
-          <p className="text-base-bold">Cart (0)</p>
+          <p className="text-base-bold">Cart ({cart.cartItems.length})</p>
         </Link>
 
         <Menu
@@ -65,7 +102,7 @@ const Navbar = () => {
               className="flex items-center gap-3 border rounded-lg px-2 py-1 hover:bg-black hover:text-white"
             >
               <ShoppingCart />
-              <p className="text-base-bold">Cart (0)</p>
+              <p className="text-base-bold">Cart ({cart.cartItems.length})</p>
             </Link>
           </div>
         )}
