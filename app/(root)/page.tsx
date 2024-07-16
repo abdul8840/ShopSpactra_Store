0e04d43148d5211d01557add19a6a410fb039c4d
abdul8840 <abdul14941naseer@gmail.com>
2024-07-16 12:18:48 +0530

    add banner to home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,11 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div>
-      Home
-    </div>
+    <>
+      <Image src="/banner.png" alt="banner" width={2000} height={1000} className="w-screen" />
+    </>
   );
 }
+
+export const dynamic = "force-dynamic";
