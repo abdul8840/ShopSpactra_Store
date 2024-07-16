f3cff51a381156b7eb22340e985d41eb765cda62
abdul8840 <abdul14941naseer@gmail.com>
2024-07-16 12:51:41 +0530

    add and display collection on home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,12 @@
+import Collections from "@/components/Collections";
+
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <Image src="/banner.png" alt="banner" width={2000} height={1000} className="w-screen" />
+      <Collections />
     </>
   );
 }
