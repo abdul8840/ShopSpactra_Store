ce559742814ca99a5111f06190990698da8e2bb9
abdul8840 <abdul14941naseer@gmail.com>
2024-07-17 18:13:02 +0530

    update wishlist again

--- app/(root)/wishlist/page.tsx
+++ app/(root)/wishlist/page.tsx
@@ -4,7 +4,7 @@ import Loader from "@/components/Loader"
 import ProductCard from "@/components/ProductCard"
 import { getProductDetails } from "@/lib/actions/actions"
 import { useUser } from "@clerk/nextjs"
-import { use, useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 
 const Wishlist = () => {
   const { user } = useUser()
@@ -20,7 +20,8 @@ const Wishlist = () => {
       setSignedInUser(data)
       setLoading(false)
     } catch (err) {
-      console.log("[users_GET", err)
+      console.error("[users_GET]", err)
+      setLoading(false)
     }
   }
 
@@ -31,30 +32,31 @@ const Wishlist = () => {
   }, [user])
 
   const getWishlistProducts = async () => {
-    setLoading(true)
-
     if (!signedInUser) return
 
-    const wishlistProducts = await Promise.all(signedInUser.wishlist.map(async (productId) => {
-      const res = await getProductDetails(productId)
-      return res
-    }))
-
-    setWishlist(wishlistProducts)
-    setLoading(false)
+    setLoading(true)
+    try {
+      const wishlistProducts = await Promise.all(signedInUser.wishlist.map(async (productId) => {
+        const res = await getProductDetails(productId)
+        return res
+      }))
+      setWishlist(wishlistProducts)
+    } catch (err) {
+      console.error("[wishlist_GET]", err)
+    } finally {
+      setLoading(false)
+    }
   }
 
   useEffect(() => {
     if (signedInUser) {
       getWishlistProducts()
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [signedInUser])
 
   const updateSignedInUser = (updatedUser: UserType) => {
-    setSignedInUser(updatedUser)
+    setSignedInUser(prevUser => ({ ...prevUser, ...updatedUser }))
   }
-
 
   return loading ? <Loader /> : (
     <div className="px-10 py-5">
@@ -62,10 +64,9 @@ const Wishlist = () => {
       {wishlist.length === 0 && (
         <p>No items in your wishlist</p>
       )}
-
       <div className="flex flex-wrap justify-center gap-16">
         {wishlist.map((product) => (
-          <ProductCard key={product._id} product={product} updateSignedInUser={updateSignedInUser}/>
+          <ProductCard key={product._id} product={product} updateSignedInUser={updateSignedInUser} />
         ))}
       </div>
     </div>
