31f8ca17e5db1befbd8a4cea4569efe8d1754704
abdul8840 <abdul14941naseer@gmail.com>
2024-07-17 18:22:37 +0530

    update wishlist page

--- app/(root)/wishlist/page.tsx
+++ app/(root)/wishlist/page.tsx
@@ -1,78 +1,47 @@
-"use client"
+import User from "@/lib/models/User";
+import { connectToDB } from "@/lib/mongoDB";
 
-import Loader from "@/components/Loader"
-import ProductCard from "@/components/ProductCard"
-import { getProductDetails } from "@/lib/actions/actions"
-import { useUser } from "@clerk/nextjs"
-import { useEffect, useState } from "react"
+import { auth } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+import { NextRequest, NextResponse } from "next/server";
 
-const Wishlist = () => {
-  const { user } = useUser()
+export const POST = async (req: NextRequest) => {
+  try {
+    const { userId } = auth()
 
-  const [loading, setLoading] = useState(true)
-  const [signedInUser, setSignedInUser] = useState<UserType | null>(null)
-  const [wishlist, setWishlist] = useState<ProductType[]>([])
-
-  const getUser = async () => {
-    try {
-      const res = await fetch("/api/users")
-      const data = await res.json()
-      setSignedInUser(data)
-      setLoading(false)
-    } catch (err) {
-      console.error("[users_GET]", err)
-      setLoading(false)
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 })
     }
+
+    await connectToDB()
+
+    const user = await User.findOne({ clerkId: userId })
+
+    if (!user) {
+      return new NextResponse("User not found", { status: 404 })
+    }
+
+    const { productId } = await req.json()
+
+    if (!productId) {
+      return new NextResponse("Product Id required", { status: 400 })
+    }
+
+    const isLiked = user.wishlist.includes(productId)
+
+    if (isLiked) {
+      // Dislike
+      user.wishlist = user.wishlist.filter((id: string) => id !== productId)
+    } else {
+      // Like
+      user.wishlist.push(productId)
+    }
+
+    await user.save()
+    
+    return NextResponse.json(user, { status: 200 })
+  } catch (err) {
+    console.log("[wishlist_POST]", err);
+    return new NextResponse("Internal Server Error", { status: 500 });
   }
-
-  useEffect(() => {
-    if (user) {
-      getUser()
-    }
-  }, [user])
-
-  const getWishlistProducts = async () => {
-    if (!signedInUser) return
-
-    setLoading(true)
-    try {
-      const wishlistProducts = await Promise.all(signedInUser.wishlist.map(async (productId) => {
-        const res = await getProductDetails(productId)
-        return res
-      }))
-      setWishlist(wishlistProducts)
-    } catch (err) {
-      console.error("[wishlist_GET]", err)
-    } finally {
-      setLoading(false)
-    }
-  }
-
-  useEffect(() => {
-    if (signedInUser) {
-      getWishlistProducts()
-    }
-  }, [signedInUser])
-
-  const updateSignedInUser = (updatedUser: UserType) => {
-    setSignedInUser(prevUser => ({ ...prevUser, ...updatedUser }))
-  }
-
-  return loading ? <Loader /> : (
-    <div className="px-10 py-5">
-      <p className="text-heading3-bold my-10">Your Wishlist</p>
-      {wishlist.length === 0 && (
-        <p>No items in your wishlist</p>
-      )}
-      <div className="flex flex-wrap justify-center gap-16">
-        {wishlist.map((product) => (
-          <ProductCard key={product._id} product={product} updateSignedInUser={updateSignedInUser} />
-        ))}
-      </div>
-    </div>
-  )
 }
-
-export const dynamic = "force-dynamic";
-
-export default Wishlist
